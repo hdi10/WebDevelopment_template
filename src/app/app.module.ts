acf1e39285837f9e502c3f493f0f620d63b6ac98
hdi10 <s0551006@htw-berlin.de>
2022-03-16 01:46:00 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiGetComponent } from './api-get/api-get.component';
+import { ApiPostComponent } from './api-post/api-post.component';
+import { ApiDeleteComponent } from './api-delete/api-delete.component';
+import { HomeComponent } from './home/home.component';
+import { TabComponent } from './shared/tab/tab.component';
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AngularMaterialModule} from "./angular-material/angular-material.module";
+import {MatIconModule} from "@angular/material/icon";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApiGetComponent,
+    ApiPostComponent,
+    ApiDeleteComponent,
+    HomeComponent,
+    TabComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        AngularMaterialModule,
+        MatIconModule,
+        MatProgressSpinnerModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
