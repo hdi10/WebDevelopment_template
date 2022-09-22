e7a3c21d5a149295920286459e5396676514bc7b
Harun Dastekin <harun.dastekin@student.htw-berlin.de>
2022-09-22 09:57:34 +0200

    experimented with animation

--- src/app/einkaufsliste/einkaufsliste.component.ts
+++ src/app/einkaufsliste/einkaufsliste.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EinkaufslisteComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    
+   }
 
   ngOnInit(): void {
+
+    //class that takes callbackfunction in its constructor!!!
+    // observes multiple elements (here entries) at the same time
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        }
+
+        //multiple or once?? ->
+        else {
+          entry.target.classList.remove('show');
+        }
+      });
+    });
+
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+    
   }
 
 }
