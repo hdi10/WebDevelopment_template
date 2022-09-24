52755cd2886d3357fa510c20171eb22d419cf2a7
Harun Dastekin <harun.dastekin@student.htw-berlin.de>
2022-09-24 15:06:30 +0200

    eye movement following mousemovement of user: finished; work login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 
+import { AngularMaterialModule } from '../angular-material/angular-material.module';
+
 
 declare const myTest: any;
 var login_attempts = 3;
+
+// declare the javascript function here
+declare function thisOne(): any;
 
 @Component({
   selector: 'app-login',
@@ -28,12 +33,40 @@ export class LoginComponent implements OnInit {
 
     document.addEventListener('mousemove', (e) => {
 
-      console.log(e)
+      //console.log(e)
 
+      // mouse position
       const mouseX = e.clientX;
+      const mouseY = e.clientY;
+
+
+      // generating degrees
+      const anchor = document.getElementById('anchor');
+      const rekt = anchor!.getBoundingClientRect();
+      const anchorX = rekt.left + rekt.width / 2;
+      const anchorY = rekt.top + rekt.height / 2;
+
+      const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
+
+      console.log(angleDeg);
+
+      // rotating eyes
+      const eyes = Array.from(document.querySelectorAll('.eye') as unknown as HTMLCollectionOf<HTMLElement>)
+      eyes.forEach(eye => {
+        eye.style.transform = `rotate(${90 + angleDeg}deg)`;
+        //anchor!.style.filter = `hue-rotate(${angleDeg}deg)`;
+      })
 
 
     })
+
+    function angle(cx: number, cy: number, ex: number, ey: number) {
+      const dy = ey - cy;
+      const dx = ex - cx;
+      const rad = Math.atan2(dy, dx); // range [-PI,PI]
+      const deg = rad * 180 / Math.PI; // rad to deg, [-180,180]
+      return deg;
+    }
 
 
   }
